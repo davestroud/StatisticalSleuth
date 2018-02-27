bfcc4c4c44b15363251ad370f4bbac5396140e61
David Stroud <david@greymatter-analytics.com>
2018-02-26 22:45:12 -0600

    Add printer rods file

---
+++ PrinterRods.R
@@ -0,0 +1,6 @@
+str(case1701)
+attach(case1701)
+## EXPLORATION
+MagneticForces <- cbind(L1,L2,L3,L4,L5,L6,L7,L8,L9,L10,L11)
+mfCor <- cor(MagneticForces)
+round(mfCor,2) # Show correlations, rounded to two digits
