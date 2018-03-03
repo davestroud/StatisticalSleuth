94cca3fc0640a10be2880e49db567e0119785672
David Stroud <david@greymatter-analytics.com>
2018-03-02 19:32:12 -0600

    add x and y variables to seperate actual vs perceived.

--- LoveAndMarriage.R
+++ LoveAndMarriage.R
@@ -4,3 +4,7 @@ attach(case1702)
 
 str(case1702)
 View(case1702)
+
+## EXPLORATION
+x <- cbind(HP,WP,HC,WC) # 4 components of level of love you feel for spouse
+y <- cbind(PW,PH,CW,CH) # 4 components of level of love you perceive from spouse
